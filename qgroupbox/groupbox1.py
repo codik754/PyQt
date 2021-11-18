8bbfdcf1e5e35ddaf3f5b2e21a720adc9bc28207
codik754 <5ochta@bk.ru>
2021-11-18 15:17:14 +0700

    QGroupBox

---
+++ qgroupbox/groupbox1.py
@@ -0,0 +1,23 @@
+#Программа с QGroupBox, которая объединяет компоненты в группы
+from PyQt5 import QtWidgets
+import sys
+
+app = QtWidgets.QApplication(sys.argv)
+window = QtWidgets.QWidget()
+window.setWindowTitle('Использование QGroupBox')
+window.resize(250, 100)
+r1 = QtWidgets.QRadioButton('Да')
+r2 = QtWidgets.QRadioButton('Не знаю')
+r3 = QtWidgets.QRadioButton('Нет')
+r1.setChecked(True)
+groupBox = QtWidgets.QGroupBox('2 * 2 = 4???  ')
+hbox = QtWidgets.QHBoxLayout()
+hbox.addWidget(r1)
+hbox.addWidget(r2)
+hbox.addWidget(r3)
+groupBox.setLayout(hbox)
+vbox = QtWidgets.QVBoxLayout()
+vbox.addWidget(groupBox)
+window.setLayout(vbox)
+window.show()
+sys.exit(app.exec_())
